06d9116d203e05cbc24ce8ec5756d7d505f8e756
Kamo Petrosyan <kamo@haikson.com>
2015-06-26 01:38:00 +0600

    using sets

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 import pysitemap
-
+import datetime
 
 """
 Example script
 """
 
 if __name__=='__main__':
-    url = 'http://www.ltsvet.ru/' # url from to crawl
+    url = 'http://www.techelec.ru/' # url from to crawl
     logfile = 'errlog.log' # path to logfile
     oformat = 'xml' # output format
     crawl = pysitemap.Crawler(url=url, logfile=logfile, oformat=oformat)
+    print datetime.datetime.now()
     crawl.crawl()
+    print datetime.datetime.now()
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     version=get_version(
         major=0,
         minor=2,
-        build=2,
+        build=3,
     ),
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     include_package_data=True,
