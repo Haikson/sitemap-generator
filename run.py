347b4f7380087d87f74358c887de38e4c2f03082
Kamo Petrosyan <kamo@haikson.com>
2015-07-03 00:59:24 +0600

    0.2.8

--- run.py
+++ run.py
@@ -1,15 +1,13 @@
 import pysitemap
-import datetime
 
 """
 Example script
 """
 
 if __name__=='__main__':
-    url = 'http://www.techelec.ru/' # url from to crawl
+    url = 'http://www.ltsvet.ru/' # url from to crawl
     logfile = 'errlog.log' # path to logfile
     oformat = 'xml' # output format
-    crawl = pysitemap.Crawler(url=url, logfile=logfile, oformat=oformat)
-    print datetime.datetime.now()
+    outputfile = '/srv/www/site/sitemap.xml' # path to output file
+    crawl = pysitemap.Crawler(url=url, logfile=logfile, oformat=oformat, outputfile=outputfile)
     crawl.crawl()
-    print datetime.datetime.now()
