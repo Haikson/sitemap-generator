d6069b9caef1a7992d811ef1f99a088533f04955
suraw00t <surawut.kong@gmail.com>
2023-07-06 15:01:16 +0700

    add create dir if not exists

--- pysitemap/format_processors/text.py
+++ pysitemap/format_processors/text.py
@@ -1,18 +1,21 @@
 import asyncio
 from aiofile import AIOFile, Reader, Writer
 import logging
+import os
 
 
-class TextWriter():
-    def __init__(self, filename: str):
-        self.filename = filename
-
+class TextWriter:
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+        self.dir, self.filename = os.path.split(self.filepath)
 
     async def write(self, urls):
-        async with AIOFile(self.filename, 'w') as aiodf:
+        if self.dir and not os.path.exists(self.dir):
+            os.makedirs(self.dir)
+
+        async with AIOFile(self.filepath, "w") as aiodf:
             writer = Writer(aiodf)
 
             for url in urls:
                 await writer("{}\n".format(url))
             await aiodf.fsync()
-
